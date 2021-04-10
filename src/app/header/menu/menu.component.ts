08d70c75fe483eb0e4337989c056b42a88473d49
Mahmoud <mymafm0405@gmail.com>
2021-04-10 11:48:12 +0300

    Sub menu comes alive

--- src/app/header/menu/menu.component.ts
+++ src/app/header/menu/menu.component.ts
@@ -4,21 +4,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css'],
 })
 export class MenuComponent implements OnInit {
-
   menuItems: MenuItem[] = [
-    new MenuItem('Company', 'company', 'glyphicon glyphicon-hdd'),
-    new MenuItem('Categories', 'categories/cats', 'glyphicon glyphicon-th-large'),
-    new MenuItem('Items', 'items/its', 'glyphicon glyphicon-grain'),
-    new MenuItem('Reports', 'reports/reps', 'glyphicon glyphicon-usd'),
-    new MenuItem('Settings', 'settings/sets', 'glyphicon glyphicon-wrench')
-  ]
+    new MenuItem('Categories', 'categories', 'glyphicon glyphicon-th-large'),
+    new MenuItem('Items', 'items', 'glyphicon glyphicon-grain'),
+    new MenuItem('Reports', 'reports', 'glyphicon glyphicon-usd'),
+    new MenuItem('Settings', 'settings', 'glyphicon glyphicon-wrench'),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/header/sub-menu/sub-menu.component.ts
+++ src/app/header/sub-menu/sub-menu.component.ts
@@ -1,21 +1,63 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { MenuItem } from 'src/app/shared/menu-item.model';
 
 @Component({
   selector: 'app-sub-menu',
   templateUrl: './sub-menu.component.html',
-  styleUrls: ['./sub-menu.component.css']
+  styleUrls: ['./sub-menu.component.css'],
 })
 export class SubMenuComponent implements OnInit {
+  menuItems: MenuItem[] = [];
 
-  constructor(private router: Router, private route: ActivatedRoute) { }
+  constructor(private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    console.log(this.route.url.subscribe(
-      (res) => {
-        console.log(res[0].path);
-      }
-    ));
+    console.log(
+      this.route.url.subscribe((res) => {
+        if (res[0].path === 'categories') {
+          this.setSubMenuCats();
+        } else if (res[0].path === 'items') {
+          this.setSubMenuItems();
+        } else if (res[0].path === 'reports') {
+          this.setSubMenuReports();
+        } else if (res[0].path === 'settings') {
+          this.setSubMenuSettings();
+        }
+      })
+    );
   }
 
+  setSubMenuCats() {
+    this.menuItems = [
+      new MenuItem('Sell', 'new-sell', 'glyphicon glyphicon-briefcase'),
+      new MenuItem('Category', 'add-category', 'glyphicon glyphicon-plus'),
+    ];
+  }
+
+  setSubMenuItems() {
+    this.menuItems = [
+      new MenuItem('Item', 'add-item', 'glyphicon glyphicon-plus'),
+    ];
+  }
+
+  setSubMenuReports() {
+    this.menuItems = [
+      new MenuItem(
+        'All Bills',
+        'all-bills',
+        'glyphicon glyphicon-modal-window'
+      ),
+      new MenuItem('By Date', 'by-date', 'glyphicon glyphicon-calendar'),
+      new MenuItem('Find', 'by-serial', 'glyphicon glyphicon-search'),
+    ];
+  }
+
+  setSubMenuSettings() {
+    this.menuItems = [
+      new MenuItem('Company', 'add-company', 'glyphicon glyphicon-plus'),
+      new MenuItem('Users', 'users', 'glyphicon glyphicon-user'),
+      new MenuItem('Permissions', 'permissions', 'glyphicon glyphicon-ok'),
+    ];
+  }
 }
