282be82e5be46e0348a3c1767d8bd7c4828f517d
Mahmoud <mymafm0405@gmail.com>
2021-04-12 10:55:00 +0300

    Reorganized

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,13 @@ import { MenuComponent } from './header/menu/menu.component';
 import { MenuItemComponent } from './header/menu-item/menu-item.component';
 import { SubMenuComponent } from './header/sub-menu/sub-menu.component';
 import { MainComponent } from './content/main/main.component';
+import { AddCategoryComponent } from './categories/add-category/add-category.component';
 
 const appRoutes: Routes = [
   {
     path: 'categories',
     component: MainComponent,
+    children: [{ path: 'add-category', component: AddCategoryComponent }],
   },
   { path: 'items', component: MainComponent },
   { path: 'reports', component: MainComponent },
@@ -30,6 +32,7 @@ const appRoutes: Routes = [
     MenuItemComponent,
     SubMenuComponent,
     MainComponent,
+    AddCategoryComponent,
   ],
   imports: [
     BrowserModule,
